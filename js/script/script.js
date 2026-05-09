302c1305f89890e5cf9df97616c85b1a0ccba83a
Daniel Rodriguez <danirodz.f@gmail.com>
2026-05-09 02:59:00 -0300

    Cambios con JS en las cards del Index

--- js/script/script.js
+++ js/script/script.js
@@ -19,4 +19,22 @@ function animarContador(id, target) {
     animarContador("stat-satisfaccion", 88);
 
     
-    
+const cardsIndex = document.querySelector(".cards-index");
+
+cardsIndex.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-primary")){
+        const card = event.target.closest(".card");
+       const cardExtra = card.querySelector(".card-extra");
+       cardExtra.classList.toggle("visible");
+       if (cardExtra.classList.contains("visible") === true) {
+        event.preventDefault();
+        card.style.height = "auto";
+        event.target.textContent = "Ver menos";
+       } else{
+        event.preventDefault();
+        card.style.height = "520px";
+        event.target.textContent = "Ver más"
+       }
+    }
+})
+
